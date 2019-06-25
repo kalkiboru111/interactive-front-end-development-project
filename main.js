95806e323511f302f1dc1f370aa4cf42dfe50120
Ubuntu <ubuntu@ip-172-31-5-104.eu-west-1.compute.internal>
2019-06-25 16:57:35 +0000

    testing API after 249 error

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-function getData(type, cb) {
+function getData(cb) {
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
+           // cb(JSON.parse(this.responseText));
+           cb(JSON.parse(xhr.responseText));
         }
     };
 
-    xhr.open("GET", "https://rest.coinapi.io/v1/exchangerate/BTC?apikey=2D2D1A7A-F554-4B2E-966A-F8585BB7A0EA");
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos");
     xhr.send();
-
+    xhr.addEventListener("load", function(response){
+        console.log(response);
+    });
 }
 
-function writeToDocument(type) {
+getData(writeToDocument);
+
+function writeToDocument(data) {
     var el = document.getElementById("data");
     el.innerHTML = " ";
-
-    getData(type, function(data) {
-        data = data.results;
-
+    
         data.forEach(function(item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
+            el.innerHTML += "<p>" + item.title + "</p>";
         });
-    });
+    
 }
