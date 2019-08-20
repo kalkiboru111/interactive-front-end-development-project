25a86c093cbf12620c9d4559163825923151bd62
Ubuntu <ubuntu@ip-172-31-5-104.eu-west-1.compute.internal>
2019-08-20 08:21:43 +0000

    Added chart functionality and tables

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
-/*
-'use strict';
+function getData(type, cb) {
 
-// Retrieiving data form URL and logging to console
-
-function getData(cb) {
     var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=10&api_key={d2b45d5710fc09775ad795e0636b58145f76f195cc96def94c38b842c49138d2}")
+    xhr.send();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            // cb(JSON.parse(this.responseText));
-            cb(JSON.parse(xhr.responseText));
+            cb(JSON.parse(this.responseText))
         }
     };
+}
 
-    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos");
-    xhr.send();
-    xhr.addEventListener("load", function(response) {
-        console.log(response);
-    });
+function getTableHeadhers(obj){
+     var tableHeaders = []
+     
+     Object.keys(obj).forEach(function(key){
+          tableHeaders.push(`<td>${key}</td>`);
+            })
+            
+    return `<tr>${tableHeaders}</tr>`;
     
 }
 
-
-// Writing API data to "data" div
-
-getData(writeToDocument);
-
-// Writing to data div
-
-function writeToDocument(data) {
-
+function writeToDocument(type){
+    var tableRows = [];
+     
     var el = document.getElementById("data");
-    el.innerHTML = " ";
+        el.innerHTML = "";
+    
+    getData(type, function(data){
+        data = data.Data
+        console.dir(data);
+        var tableHeaders = getTableHeadhers(data[0]);
 
-    data.forEach(function(item) {
-        el.innerHTML += "<p>" + item.title + "</p>";
-    });
+        data.forEach(function(item){
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key){
+                dataRow.push(`<td>${item[key]}</td>`);
+            })
+            
+            tableRows.push(`<tr>${dataRow}</tr>`);
+            
+            // document.getElementById("data").innerHTML += "<p>" + item.time + "</p>";
+            el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`.replace(/,/g, "");
+            
+        })
+        
+        
+    })
+   
     
 }
 
-// 
-*/
+
+
